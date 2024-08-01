eb53063b8d7d4a7cd524342bc281fab771319eff
MickaMH <mika80830@hotmail.fr>
2024-08-01 20:06:31 +0200

    jeu 01/08 20:00

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,81 +2,95 @@
 
 namespace App\Controller;
 
-use App\Entity\Utilisateur;
-use App\Form\FacturationType;
-use App\Form\LivraisonType;
+use App\Entity\Commande;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Part\DataPart;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TestController extends AbstractController
 {
     #[Route('/test', name: 'app_test')]
-    public function index(Request $request, EntityManagerInterface $entityManager, Security $security): Response
-    {
-    $formFacturation = $this->createForm(FacturationType::class);
-    $formLivraison = $this->createForm(LivraisonType::class);
+public function index(EntityManagerInterface $entityManager, MailerInterface $mailer): Response
+{
+    $lastOrder = $this->getLastOrder($entityManager);
+    $plats = [];
 
-    if ($request->isMethod('POST')) {
-        $this->adresseFacturation($request, $entityManager, $security, $formFacturation);
-        $this->adresseLivraison($request, $entityManager, $security, $formLivraison);
-        return $this->redirectToRoute('app_test');
+    if ($lastOrder) {
+        $commande = [
+            // Récupérez les données de la commande que vous souhaitez envoyer par email
+            'id' => $lastOrder->getId(),
+            'date' => $lastOrder->getDateCommande(),
+            // ...
+        ];
+
+        // Récupérez les détails de la commande
+        $details = $lastOrder->getDetails();
+
+        // Envoyer un email à l'utilisateur avec les informations de sa commande
+        // $this->sendNewConfirmationEmail($commande, $details, $mailer);
     }
 
+    $plats = $this->getPlatsFromDetails($details);
+    dump($plats); // Add this line
     return $this->render('test/index.html.twig', [
-        'controller_name' => 'TestController',
-        'formFacturation' => $formFacturation,
-        'formLivraison' => $formLivraison
-    ]);
-    }
+    'commande' => $commande ?? [],
+    'details' => $details ?? [],
+    'plats' => $plats,
+]);
+}
 
-public function adresseFacturation(Request $request, EntityManagerInterface $entityManager, Security $security, FormInterface $formFacturation): void
+    private function getLastOrder(EntityManagerInterface $entityManager): ?Commande
     {
-    $formFacturation->handleRequest($request);
+        if ($this->getUser()) {
+            $userId = $this->getUser()->getId();
+            $lastOrder = $entityManager->getRepository(Commande::class)->findOneBy(['utilisateur' => $userId], ['id' => 'DESC']);
 
-    if ($formFacturation->isSubmitted() && $formFacturation->isValid()) {
-        // Traitement des données du formulaire de facturation
-        $dataFacturation = $formFacturation->getData();
+            return $lastOrder;
+        }
 
-        // Met à jour l'adresse de facturation de l'utilisateur
-        $utilisateur = $security->getUser();
-        $adresseFacturation = $dataFacturation->getAdresseFacturation();
-        $utilisateur->setAdresseFacturation($adresseFacturation);
-
-        $cpFacturation = $dataFacturation->getCpFacturation();
-        $utilisateur->setCpFacturation($cpFacturation);
-
-        $villeFacturation = $dataFacturation->getVilleFacturation();
-        $utilisateur->setVilleFacturation($villeFacturation);
-
-        $entityManager->flush();
-    }
+        return null;
     }
 
-public function adresseLivraison(Request $request, EntityManagerInterface $entityManager, Security $security, FormInterface $formLivraison): void
+    private function getPlatsFromDetails(Collection $details): array
+{
+    $plats = [];
+    foreach ($details as $detail) {
+        dump($detail); // Add this line
+        $plat = $detail->getPlat();
+        if ($plat) {
+            dump($plat); // Add this line
+            $plats[] = [
+                'id' => $plat->getId(),
+                'libelle' => $plat->getLibelle(),
+                'prix' => $plat->getPrix(),
+                'image' => $detail->getImage(),
+            ];
+        }
+    }
+    return $plats;
+}
+
+    private function sendNewConfirmationEmail(array $commande, Collection $details, MailerInterface $mailer)
     {
-    $formLivraison->handleRequest($request);
+        if ($this->getUser()) {
+            $emailAddress = $this->getUser()->getUserIdentifier();
+            $email = (new Email())
+                ->from('TheDistrict@gmail.com') // Remplacez par votre adresse email
+                ->to($emailAddress)
+                ->subject('Récapitulatif de commande')
+                ->html($this->renderView('new_confirmation.html.twig', [
+                    'commande' => $commande,
+                    'details' => $details,
+                ]))
+                ->addPart((new DataPart(fopen('C:\xampp\htdocs\disctrict_symfony\public\images\district\logo.webp', 'r'), 'logo', 'image/webp'))->asInline());
+            // ->addPart((new DataPart(fopen('C:\xampp\htdocs\disctrict_symfony\public\images\plats\image_plat_vierge.webp', 'r'), 'plat', 'image/webp'))->asInline());
 
-    if ($formLivraison->isSubmitted() && $formLivraison->isValid()) {
-        // Traitement des données du formulaire de livraison
-        $dataLivraison = $formLivraison->getData();
-
-        // Met à jour l'adresse de livraison de l'utilisateur
-        $utilisateur = $security->getUser();
-        $adresseLivraison = $dataLivraison->getAdresseLivraison();
-        $utilisateur->setAdresseLivraison($adresseLivraison);
-
-        $cpLivraison = $dataLivraison->getCpLivraison();
-        $utilisateur->setCpLivraison($cpLivraison);
-
-        $villeLivraison = $dataLivraison->getVilleLivraison();
-        $utilisateur->setVilleLivraison($villeLivraison);
-
-        $entityManager->flush();
-    }
+            $mailer->send($email);
+        }
     }
 }
