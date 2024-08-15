6f2819cde8634cff2820ff46e387b97de59d1e21
MickaMH <mika80830@hotmail.fr>
2024-08-15 19:43:11 +0200

    jeu 15/08 20:00

--- src/Controller/CompteController.php
+++ src/Controller/CompteController.php
@@ -11,8 +11,9 @@ use App\Repository\PlatRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 // use Symfony\Bundle\SecurityBundle\Security;
-// use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CompteController extends AbstractController
@@ -53,6 +54,51 @@ class CompteController extends AbstractController
     ]);
     }
 
+
+
+    #[Route('/compte/modifPassword', name: 'app_compte_modifPassword')]
+    public function changerMotDePasse(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $oldPassword = $request->get('old_password');
+        $newPassword = $request->get('new_password');
+        $confirmNewPassword = $request->get('confirm_new_password');
+
+        // Vérifier que l'ancien mot de passe est correct
+        if (!$passwordHasher->isPasswordValid($user, $oldPassword)) {
+            // L'ancien mot de passe est incorrect
+            $this->addFlash('error', 'Ancien mot de passe incorrect');
+            return $this->redirectToRoute('app_compte');
+        }
+
+        // Vérifier que les deux nouveaux mots de passe sont identiques
+        if ($newPassword !== $confirmNewPassword) {
+            // Les deux nouveaux mots de passe ne sont pas identiques
+            $this->addFlash('error', 'Les deux nouveaux mots de passe ne sont pas identiques');
+            return $this->redirectToRoute('app_compte');
+        }
+
+        // Vérifier que le nouveau mot de passe contient au moins 6 caractères
+        if (strlen($newPassword) < 6) {
+        // Le nouveau mot de passe est trop court
+        $this->addFlash('error', 'Le nouveau mot de passe doit contenir au moins 6 caractères');
+        return $this->redirectToRoute('app_compte');
+        }
+
+        // Hasher le nouveau mot de passe
+        $newPasswordHash = $passwordHasher->hashPassword($user, $newPassword);
+
+        // Mettre à jour le mot de passe de l'utilisateur
+        $user->setPassword($newPasswordHash);
+        $entityManager->flush();
+
+        // Afficher un message de confirmation
+        $this->addFlash('success', 'Mot de passe mis à jour avec succès');
+        return $this->redirectToRoute('app_compte');
+    }
+
+
+
     #[Route('/compte/supprimer', name: 'app_compte_supprimer')]
     public function supprimerCompte(EntityManagerInterface $entityManager): Response
     {
@@ -79,11 +125,11 @@ class CompteController extends AbstractController
     $entityManager->remove($user);
     $entityManager->flush();
 
-
     // Déconnexion de l'utilisateur
     $this->container->get('security.token_storage')->setToken(null);
 
         return $this->redirectToRoute('app_confirm_suppression');
     }
+
 
 }
