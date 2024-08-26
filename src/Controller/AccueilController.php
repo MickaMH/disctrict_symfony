fd36ad12711a3b8b44fd78caeb5c1eaa8fe286ab
MickaMH <mika80830@hotmail.fr>
2024-08-26 19:13:12 +0200

    lun 26/08 19:15

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,35 +2,89 @@
 
 namespace App\Controller;
 
-use App\Repository\CategorieRepository;
+use App\Repository\DetailRepository;
 use App\Repository\PlatRepository;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AccueilController extends AbstractController
 {
-    private $categorieRepo;
-    private $platRepo;
-
-    public function __construct(CategorieRepository $categorieRepo, PlatRepository $platRepo)
-    {
-        $this->categorieRepo = $categorieRepo;
-        $this->platRepo = $platRepo;
-    }
-
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(DetailRepository $detailRepository, PlatRepository $platRepository): Response
     {
-        $categories = $this->categorieRepo->findBy(['libelle' => ['burger', 'wrap', 'pâtes']]);
-        $plats = $this->platRepo->findBy(['libelle' => ['boisson gazeuse', 'salade cesar', 'burger cheese']]);
+        $top3Plats = $this->getTop3Plats($detailRepository);
+        $top3Categories = $this->getTop3Categories($detailRepository, $platRepository);
 
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
-
-            'categories' => $categories,
-            'plats' => $plats,
+            'top3Plats' => $top3Plats,
+            'top3Categories' => $top3Categories,
         ]);
+    }
+
+    private function getTop3Plats(DetailRepository $detailRepository): array
+    {
+        // Récupère les détails triés par quantité décroissante
+        $details = $detailRepository->findBy([], ['quantite' => 'DESC']);
+
+        // Prépare un tableau pour stocker les plats avec leur quantité totale
+        $platsAvecQuantite = [];
+
+        // Boucle sur chaque détail
+        foreach ($details as $detail) {
+            $plat = $detail->getPlats();
+            if (isset($platsAvecQuantite[$plat->getId()])) {
+                $platsAvecQuantite[$plat->getId()]['quantite'] += $detail->getQuantite();
+            } else {
+                $platsAvecQuantite[$plat->getId()] = [
+                    'plat' => $plat, // Pass the entire Plat object
+                    'quantite' => $detail->getQuantite(),
+                ];
+            }
+        }
+
+        // Trie les plats par quantité décroissante
+        uasort($platsAvecQuantite, function($a, $b) {
+            return $b['quantite'] <=> $a['quantite'];
+        });
+
+        // Extrait les 3 premiers éléments du tableau
+        $top3Plats = array_slice($platsAvecQuantite, 0, 3);
+
+        return $top3Plats;
+    }
+
+    private function getTop3Categories(DetailRepository $detailRepository, PlatRepository $platRepository): array
+    {
+        // Récupère les détails triés par quantité décroissante
+        $details = $detailRepository->findBy([], ['quantite' => 'DESC']);
+
+        // Prépare un tableau pour stocker les catégories avec leur quantité totale
+        $categoriesAvecQuantite = [];
+
+        // Boucle sur chaque détail
+        foreach ($details as $detail) {
+            $plat = $detail->getPlats();
+            $categorie = $plat->getCategories();
+            if (isset($categoriesAvecQuantite[$categorie->getId()])) {
+                $categoriesAvecQuantite[$categorie->getId()]['quantite'] += $detail->getQuantite();
+            } else {
+                $categoriesAvecQuantite[$categorie->getId()] = [
+                    'categorie' => $categorie, // Pass the entire Categorie object
+                    'quantite' => $detail->getQuantite(),
+                ];
+            }
+        }
+
+        // Trie les catégories par quantité décroissante
+        uasort($categoriesAvecQuantite, function($a, $b) {
+            return $b['quantite'] <=> $a['quantite'];
+        });
+
+        // Extrait les 3 premiers éléments du tableau
+        $top3Categories = array_slice($categoriesAvecQuantite, 0, 3);
+
+        return $top3Categories;
     }
 }
--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,121 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Commande;
-use App\Entity\Detail;
-use App\Form\AdressesDesktopType;
-use App\Repository\PlatRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TestController extends AbstractController
-
 {
     #[Route('/test', name: 'app_test')]
-    public function index(SessionInterface $session, PlatRepository $platRepository, Request $request, EntityManagerInterface $entityManager, Security $security): Response
-    
+    public function index(): Response
     {
-        $panier = $session->get('panier', []);
-
-    if (empty($panier)) {
-        $this->addFlash('message', 'Votre panier est vide !');
-        return $this->redirectToRoute('app_panier');
-    } else {
-        $this->denyAccessUnlessGranted('ROLE_USER');
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
     }
-
-    $panierWithData = $this->resumeCommande($session, $platRepository);
-
-    $formAdressesDesktop = $this->createForm(AdressesDesktopType::class);
-
-    $formAdressesDesktop->handleRequest($request);
-
-    if ($formAdressesDesktop->isSubmitted() && $formAdressesDesktop->isValid()) {
-        $dataFacturation = $formAdressesDesktop->getData();
-
-        $commande = new Commande();
-
-        $adresseFacturation = $dataFacturation->getAdresseFacturation();
-        $commande->setAdresseFacturation($adresseFacturation);
-
-        $cpFacturation = $dataFacturation->getCpFacturation();
-        $commande->setCpFacturation($cpFacturation);
-
-        $villeFacturation = $dataFacturation->getVilleFacturation();
-        $commande->setVilleFacturation($villeFacturation);
-
-        $adresseLivraison = $dataFacturation->getAdresseLivraison();
-        $commande->setAdresseLivraison($adresseLivraison);
-
-        $cpLivraison = $dataFacturation->getCpLivraison();
-        $commande->setCpLivraison($cpLivraison);
-
-        $villeLivraison = $dataFacturation->getVilleLivraison();
-        $commande->setVilleLivraison($villeLivraison);
-
-        $commande->setUtilisateur($this->getUser());
-        $commande->setDateCommande(new \DateTime());
-        $commande->setEtat('0');
-
-        $totalCommande = 0;
-
-        foreach ($panier as $item => $quantite) {
-            $detail = new Detail();
-
-            $plat = $platRepository->find($item);
-
-            $detail->setQuantite($quantite);
-            $detail->setPlats($plat);
-            $detail->setCommande($commande);
-
-            $sousTotal = $quantite * $plat->getPrix();
-            $totalCommande += $sousTotal;
-
-            $commande->addDetail($detail);
-        }
-
-        $commande->setTotal($totalCommande);
-
-        $entityManager->persist($commande);
-        $entityManager->flush();
-
-        $session->remove('panier');
-
-        $this->addFlash('message', 'Commande créée avec succès');
-        return $this->redirectToRoute('app_confirm_commande');
-    }
-
-    return $this->render('test/index.html.twig', [
-        'controller_name' => 'TestController',
-        'panierWithData' => $panierWithData,
-        'formAdressesDesktop' => $formAdressesDesktop,
-    ]);
-}
-
-public function resumeCommande(SessionInterface $session, PlatRepository $platRepository): array
-{
-    $panier = $session->get('panier', []);
-
-    $panierWithData = [];
-
-    foreach ($panier as $id => $quantite) {
-        $plat = $platRepository->find($id);
-        if ($plat) {
-            $image = $plat->getImage();
-            $panierWithData[] = [
-                'plat' => $plat,
-                'quantite' => $quantite,
-                'image' => $image
-            ];
-        }
-    }
-
-    return $panierWithData;
-}
 }
